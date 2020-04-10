818c939d7f7b07f1a6c4e2367f304d313992a5c1
Arnab <arnab4srk@gmail.com>
2020-04-10 22:56:49 +0530

    Changed API

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-$.getJSON('https://corona.lmao.ninja/countries/IND', function(data) 
+$.getJSON('https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise', function(data) 
 {
-    var total_cases = data.cases
-     var deaths = data.deaths
-     var recovered = data.recovered
-     var new_cases = data.todayCases
+    var total_cases = data.data.total.confirmed;
+    var deaths =  data.data.total.deaths;
+    var recovered =  data.data.total.recovered;
+    var active = data.data.total.active;
     $(".total_cases").html(total_cases);
     $(".deaths").html(deaths);
     $(".recovered").html(recovered);
-    $(".new_cases").html(new_cases);
+    $(".active").html(active);
 });
